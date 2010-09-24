e2dcb1927e4718facc1077874bf09dc43bd9a8d3
Jiffer <jeffrey.young@trincoll.edu>
2010-09-24 12:21:02 -0400

    added drawing for mouse, but css messing it up

--- JS/MotionSenseCanvas.js
+++ JS/MotionSenseCanvas.js
@@ -1,12 +1,55 @@
-function draw() {
-    var canvas = document.getElementById("input_canvas");
-    if (canvas.getContext) {
-        var ctx = canvas.getContext("2d");
- 
-        ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect (10, 10, 55, 50);
- 
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect (30, 30, 55, 50);
-    }
-}
+// onload function....
+if(window.addEventListener)
+    {
+    window.addEventListener('load', function ()
+			    {
+	    
+	    var canvas;
+
+	    var context;
+
+	    /**
+	       Draw the initial canvas, and init everything
+	    */
+
+	    function draw()
+	    {
+		/**
+		   Grab the canvas
+		*/
+		canvas = document.getElementById('input_canvas');
+
+		context = canvas.getContext('2d');
+
+		canvas.addEventListener('mousemove', ev_mousemove, false);
+	    }
+
+	    var started = false;
+	    function ev_mousemove(ev)
+	    {
+		var x, y;
+
+		
+		if(ev.layerX || ev.layerX == 0)
+		    { // Firefox ish
+		    x = ev.layerX;
+		    y = ev.layerY;
+		} else if(ev.offsetX || ev.offsetX == 0)
+		    { // Opera  ish
+		    x = ev.offsetX;
+		    y = ev.offsetY;
+		}
+
+		if (!started) {
+		    context.beginPath();
+		    context.moveTo(x, y);
+		    started = true;
+		} else {
+		    context.lineTo(x, y);
+		    context.stroke();
+		}
+	    }
+
+	    draw();
+	}, false);
+ }
