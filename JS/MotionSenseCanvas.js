54baba74047f1b55950e747e95b18296cbb5c2df
Jiffer <jeffrey.young@trincoll.edu>
2010-09-25 14:34:29 -0400

    added mouse down, up events

--- JS/MotionSenseCanvas.js
+++ JS/MotionSenseCanvas.js
@@ -1,55 +1,88 @@
-// onload function....
+// Keep everything in anonymous function, called on window load.
 if(window.addEventListener)
     {
-    window.addEventListener('load', function ()
+    window.addEventListener('load', function()
 			    {
-	    
+
 	    var canvas;
 
 	    var context;
 
-	    /**
-	       Draw the initial canvas, and init everything
-	    */
+	    var pointer;
 
 	    function draw()
 	    {
-		/**
-		   Grab the canvas
-		*/
+	       
 		canvas = document.getElementById('input_canvas');
-
+		
 		context = canvas.getContext('2d');
-
-		canvas.addEventListener('mousemove', ev_mousemove, false);
-	    }
-
-	    var started = false;
-	    function ev_mousemove(ev)
-	    {
-		var x, y;
+		
 
 		
-		if(ev.layerX || ev.layerX == 0)
-		    { // Firefox ish
-		    x = ev.layerX;
-		    y = ev.layerY;
-		} else if(ev.offsetX || ev.offsetX == 0)
-		    { // Opera  ish
-		    x = ev.offsetX;
-		    y = ev.offsetY;
+		tool = new pointer();
+
+		canvas.addEventListener('mousedown', get_ev_canvas, false);
+
+		canvas.addEventListener('mousemove', get_ev_canvas, false);
+
+		canvas.addEventListener('mouseup',   get_ev_canvas, false);
+
+	    }
+
+	  
+	    function pointer() 
+	    {
+
+		var location = this;
+		this.started = false;
+
+		this.mousedown = function(ev)
+		    {
+		    context.beginPath();
+		    context.moveTo(ev._x, ev._y);
+		    tool.started = true;
+		};
+
+		
+		this.mousemove = function(ev)
+		    {
+		    if(tool.started)
+			{
+			context.lineTo(ev._x, ev._y);
+			context.stroke();
+		    }
+		};
+
+		this.mouseup = function(ev)
+		    {
+		    if (tool.started)
+			{
+			tool.mousemove(ev);
+			tool.started = false;
+		    }
+		};
+	    }
+
+	    function get_ev_canvas(ev)
+	    {
+		if(ev.layerX || ev.layerX == 0) 
+		    { // gecko
+		    ev._x = ev.layerX;
+		    ev._y = ev.layerY;
+		}
+		else if(ev.offsetX || ev.offsetX == 0)
+		    { // Opera
+		    ev._x = ev.offsetX;
+		    ev._y = ev.offsetY;
 		}
 
-		if (!started) {
-		    context.beginPath();
-		    context.moveTo(x, y);
-		    started = true;
-		} else {
-		    context.lineTo(x, y);
-		    context.stroke();
+		var func = tool[ev.type];
+		if(func)
+		    {
+		    func(ev);
 		}
 	    }
 
 	    draw();
-	}, false);
- }
+
+	}, false); }
